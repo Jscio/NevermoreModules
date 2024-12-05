b09cca5d7f6c4dc59a6c4ecfebe6dffef336aefa
Jscio <120773070+Jscio@users.noreply.github.com>
2024-12-05 22:14:19 +0700

    fix: fix a couple of syntax errors on loader.lua

--- loader.lua
+++ loader.lua
@@ -4,34 +4,32 @@ local loadedModules = getgenv().NVRMR_LOADED or {}
 getgenv().NVRMR_LOADED = loadedModules
 
 local nvmrModulesList = {
-  "promise" = "https://raw.githubusercontent.com/Jscio/NevermoreModules/main/modules/promise.lua",
-  "maid" = "https://raw.githubusercontent.com/Jscio/NevermoreModules/main/modules/maid.lua",
-  "signal" = "https://raw.githubusercontent.com/Jscio/NevermoreModules/main/modules/signal.lua",
-  "deferred" = "https://raw.githubusercontent.com/Jscio/NevermoreModules/main/modules/deferred.lua",
-  "event-handler-utils" = "https://raw.githubusercontent.com/Jscio/NevermoreModules/main/modules/event-handler-utils.lua"
+  promise = "https://raw.githubusercontent.com/Jscio/NevermoreModules/main/modules/promise.lua",
+  maid = "https://raw.githubusercontent.com/Jscio/NevermoreModules/main/modules/maid.lua",
+  signal = "https://raw.githubusercontent.com/Jscio/NevermoreModules/main/modules/signal.lua",
+  deferred = "https://raw.githubusercontent.com/Jscio/NevermoreModules/main/modules/deferred.lua",
+  ["event-handler-utils"] = "https://raw.githubusercontent.com/Jscio/NevermoreModules/main/modules/event-handler-utils.lua"
 }
 
-if not getgenv().NVRMR_REQUIRE then
-  function getgenv().NVRMR_REQUIRE(moduleName: string)
-      if not nvmrModulesList[moduleName] then
-          warn(string.format("[NVRMR]: Module '%s' not found in the module list", moduleName))
-          return nil
-      end
+getgenv().NVRMR_REQUIRE = function(moduleName: string)
+    if not nvmrModulesList[moduleName] then
+        warn(string.format("[NVRMR]: Module '%s' not found in the module list", moduleName))
+        return nil
+    end
 
-      if loadedModules[moduleName] then
-          return loadedModules[moduleName]
-      end
+    if loadedModules[moduleName] then
+        return loadedModules[moduleName]
+    end
 
-      local success, module = pcall(function()
-          return loadstring(game:HttpGet(nvmrModulesList[moduleName]))()
-      end)
+    local success, module = pcall(function()
+        return loadstring(game:HttpGet(nvmrModulesList[moduleName]))()
+    end)
 
-      if not success then
-          warn(string.format("[NVRMR]: Failed to load module '%s'. Error: %s", moduleName, module))
-          return nil
-      end
+    if not success then
+        warn(string.format("[NVRMR]: Failed to load module '%s'. Error: %s", moduleName, module))
+        return nil
+    end
 
-      loadedModules[moduleName] = module
-      return module
-  end
+    loadedModules[moduleName] = module
+    return module
 end
